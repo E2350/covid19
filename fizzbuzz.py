4cb5d153608da4785f6b170ea7c0884b9cf36d1a
Mzfr <muzaffer.ayd38@gmail.com>
2020-07-17 16:31:48 +0300

    FizzBuzz

---
+++ fizzbuzz.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+number=list(range(1,101))
+
+
+for i in number:
+    if i % 3 == 0 and i % 5 == 0:
+        print("FizzBuzz")
+    elif i % 3 == 0:
+        print("Fizz")
+    elif i % 5 == 0:
+        print("Buzz")
+    else:
+        print(i)
+    
+        
+
+
+        
